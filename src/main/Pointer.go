3a21ea83a419818443d99530dbb847c1d26764f7
KaserChan <chankaser@gmail.com>
2020-02-05 18:44:42 +0800

    指针

---
+++ src/main/Pointer.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var car int = 1
+	var ba string = "baba"
+	fmt.Printf("%p,%p\n", &car, &ba) //显示两个变量的地址
+
+	house := "A house refers to a string"
+	//对字符串取地址,ptr的类型为*string
+	ptr := &house
+	fmt.Printf("ptr type : %T\n", ptr)
+
+	fmt.Printf("ptr address : %p\n", ptr)
+
+	//对指针进行取值操作,value的值与house的值相同
+	value := *ptr
+
+	fmt.Printf("value type : %T\n", value)
+
+	// 在这里 house和value在内存中的地址是不一样的,这里体现当我们用一个变量来对指针取值的时候
+	// 其实是在申请一个新的变量,将其赋值为指针所对应内存地址的值
+	fmt.Printf("value & house address : %p,%p\n", &value, &house)
+
+	//用指针进行数值交换
+	a, b := 5, 6
+	fmt.Printf("before swap a,b %d,%d \t %p,%p\n", a, b, &a, &b)
+	swap(&a, &b)
+	//可以看到我们的交换并不是交换地址,而是取到地址后交换他们的值
+	fmt.Printf("after swap a,b %d,%d \t %p,%p\n", a, b, &a, &b)
+}
+
+func swap(a, b *int) {
+	//取a指针的值
+	t := *a
+	*a = *b
+	*b = t
+}
