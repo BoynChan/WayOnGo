9219113159c00da26966f463c55817a40bd5e0a6
KaserChan <chankaser@gmail.com>
2020-02-15 16:37:06 +0800

    feat(du): 改为并发遍历目录
    使用一个waitGroup进行对活跃的walkDir调用进行计数,当为0时,会将通道关闭
    使用信号量来控制并发量的大小,比较过大影响系统性能

--- src/main/Goroutines/du/walkdir.go
+++ src/main/Goroutines/du/walkdir.go
@@ -3,8 +3,10 @@ package du
 import (
 	"fmt"
 	"io/ioutil"
+	"main/Goroutines/semaphore"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -14,13 +16,21 @@ import (
 author:Boyn
 date:2020/2/15
 */
+var waitGroup sync.WaitGroup
+var s = semaphore.NewSemaphore(20)
 
 func WalkDir(initDir []string) {
+
 	fileSizes := make(chan int64)
+
+	for _, root := range initDir {
+		//增加一个计数的值
+		waitGroup.Add(1)
+		go doWalkDir(root, fileSizes)
+	}
 	go func() {
-		for _, root := range initDir {
-			doWalkDir(root, fileSizes)
-		}
+		//当n的值为0时,就会结束阻塞
+		waitGroup.Wait()
 		close(fileSizes)
 	}()
 
@@ -46,8 +56,12 @@ loop:
 }
 
 func doWalkDir(dir string, fileSizes chan<- int64) {
+	//在函数结束的时候,会将等待组的值减一
+	defer waitGroup.Done()
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
+			//如果是目录,会进入递归函数,所以将等待组的值加一
+			waitGroup.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
 			doWalkDir(subdir, fileSizes)
 		} else {
@@ -58,8 +72,13 @@ func doWalkDir(dir string, fileSizes chan<- int64) {
 
 /*
 dirents返回目录下的文件
+在并发过程中,为了防止并发数量过多
+在dirent函数中使用一个信号量作为并发量的控制
 */
 func dirents(dir string) []os.FileInfo {
+	//信号量
+	s.Acquire()
+	defer s.Release()
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
