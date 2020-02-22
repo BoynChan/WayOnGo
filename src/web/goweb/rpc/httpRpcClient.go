252778c40547abfe24dab829260d109afb62c927
KaserChan <chankaser@gmail.com>
2020-02-22 17:32:37 +0800

    feat(RPC): 基于HTTP协议的RPC调用

---
+++ src/web/goweb/rpc/httpRpcClient.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+// Author:Boyn
+// Date:2020/2/22
+
+// 基于HTTP协议传输的RPC客户端
+
+// 用于传送相乘的参数
+type Args struct {
+	A, B int
+}
+
+// 用于接收相除的结果,分别为商和余数
+type Quotient struct {
+	Quo, Rem int
+}
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", "localhost:8019")
+	if err != nil {
+		log.Fatal("连接失败:", err)
+	}
+	args := Args{17, 8}
+	var reply int
+	err = client.Call("Arith.Multiple", args, &reply)
+	if err != nil {
+		log.Fatal("调用失败:", err)
+	}
+	fmt.Printf("Arith:%d*%d=%d\n", args.A, args.B, reply)
+	var quot Quotient
+	err = client.Call("Arith.Divide", args, &quot)
+	if err != nil {
+		log.Fatal("调用失败:", err)
+	}
+	fmt.Printf("Arith:%d➗%d=%d...%d\n", args.A, args.B, quot.Quo, quot.Rem)
+
+}
---
+++ src/web/goweb/rpc/httpRpcServer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/rpc"
+)
+
+// Author:Boyn
+// Date:2020/2/22
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+// 对于RPC来说,要用 func (type *Type) funcName(args Args, reply *Reply) error 这种类型的函数才可以被识别
+func (t *Arith) Multiple(args Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+func (t *Arith) Divide(args Args, reply *Quotient) error {
+	if args.B == 0 {
+		return errors.New("除数不能为0")
+	}
+	reply.Quo = args.A / args.B
+	reply.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	// 这里要是指针
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	err := http.ListenAndServe(":8019", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
