50a8a2071a64cb5811deac10955c5275e6eb18da
KaserChan <chankaser@gmail.com>
2020-02-23 14:53:40 +0800

    feat(go-mega): 02-Template Basic
    将模板抽取成一个index.html文件,并演示了其if-else与循环操作

--- src/web/go-mega/main.go
+++ src/web/go-mega/main.go
@@ -11,17 +11,42 @@ type User struct {
 	Username string
 }
 
+type Post struct {
+	User  User
+	Title string
+}
+
+type IndexViewModel struct {
+	User  User
+	Title string
+	Posts []Post
+}
+
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		user := User{Username: "boyn"}
-		tpl, _ := template.New("").Parse(`
-		<html><head><title>Home - Boyn</title></head>
-<body>
-<h1>Hello,{{.Username}}</h1>
-</body>
-`)
-		tpl.Execute(writer, &user)
-
+		u1 := User{Username: "boyn"}
+		u2 := User{Username: "kaserchan"}
+		posts := []Post{
+			{
+				User:  u1,
+				Title: "模板的if-else操作",
+			},
+			{
+				User:  u1,
+				Title: "模板的插值操作",
+			},
+			{
+				User:  u2,
+				Title: "模板的循环操作",
+			},
+		}
+		v := IndexViewModel{
+			User:  u1,
+			Title: "Home Page",
+			Posts: posts,
+		}
+		tpl, _ := template.ParseFiles("F:\\Code\\Go\\LearningGo\\src\\web\\go-mega\\templates\\index.html")
+		tpl.Execute(writer, &v)
 	})
 	http.ListenAndServe(":8009", nil)
 }
