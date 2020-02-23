b6163a1079fdca93fb2806e5509a4036e6088d4e
KaserChan <chankaser@gmail.com>
2020-02-23 21:00:55 +0800

    feat(go-mega): 04-Web Form
    增加登录流程,在controller处,设置了处理登录的处理函数,对于GET请求,将解析的模板与VM一起返回,对于POST请求,验证输入的账号密码,如果有错误,则还是返回登录页面,并返回错误信息,如果没错误,就重定向到首页

--- src/web/go-mega/controller/home.go
+++ src/web/go-mega/controller/home.go
@@ -11,6 +11,7 @@ type home struct{}
 
 func (h home) registerRoutes() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
 }
 
 // 这个函数被registerRoutes()注册在homeController中,它注册的路径是'/'
@@ -19,4 +20,39 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	vop := vm.IndexViewModelOp{}
 	v := vop.GetVM()
 	templates["index.html"].Execute(w, &v)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	temName := "login.html"
+	vop := vm.LoginViewModelOp{}
+	v := vop.GetVM()
+	if r.Method == http.MethodGet {
+		templates[temName].Execute(w, &v)
+	} else if r.Method == http.MethodPost {
+		r.ParseForm()
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+		if len(username) < 3 {
+			v.AddError("用户名过短")
+		}
+		if len(password) < 6 {
+			v.AddError("密码过短")
+		}
+		if !check(username, password) {
+			v.AddError("用户名与密码错误")
+		}
+		if len(v.Errs) > 0 {
+			templates[temName].Execute(w, &v)
+		} else {
+			//如果密码正确,就进行302重定向
+			http.Redirect(w, r, "/", 302)
+		}
+	}
+}
+
+func check(username, password string) bool {
+	if username == "boyn" && password == "123456" {
+		return true
+	}
+	return false
 }
