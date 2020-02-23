002de91752ecaa397040ab394820d34d1f229333
KaserChan <chankaser@gmail.com>
2020-02-23 14:44:08 +0800

    feat(go-mega): 01-Template Basic
    创建了一个HTTP模板,并将我们的变量与模板中预设变量进行绑定
    类似于JSP

--- src/web/go-mega/main.go
+++ src/web/go-mega/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 // Author:Boyn
 // Date:2020/2/23
+type User struct {
+	Username string
+}
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello!"))
+		user := User{Username: "boyn"}
+		tpl, _ := template.New("").Parse(`
+		<html><head><title>Home - Boyn</title></head>
+<body>
+<h1>Hello,{{.Username}}</h1>
+</body>
+`)
+		tpl.Execute(writer, &user)
+
 	})
 	http.ListenAndServe(":8009", nil)
 }
