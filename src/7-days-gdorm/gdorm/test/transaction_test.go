86d913b5c6e7e49015d7a0ef13e63ecb0e7ecf71
BoynChan <chankaser@gmail.com>
2020-03-27 11:31:23 +0800

    feat(gdorm): transaction
    在session中加入了事务功能

---
+++ src/7-days-gdorm/gdorm/test/transaction_test.go
@@ -0,0 +1,51 @@
+package test
+
+import (
+	"7-days-gdorm/gdorm/session"
+	"errors"
+	"testing"
+)
+
+//Author: Boyn
+//Date: 2020/3/27
+
+func TestTransaction(t *testing.T) {
+	t.Run("rollback", func(t *testing.T) {
+		transactionRollback(t)
+	})
+	t.Run("commit", func(t *testing.T) {
+		transactionCommit(t)
+	})
+}
+
+func transactionRollback(t *testing.T) {
+	engine := getEngine()
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return nil, errors.New("error")
+	})
+	if err == nil || s.HasTable() {
+		t.Fail()
+	}
+}
+
+func transactionCommit(t *testing.T) {
+	engine := getEngine()
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return
+	})
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fail()
+	}
+}
